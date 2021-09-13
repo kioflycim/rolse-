cb106d945a6da219b4bcf8f51c9cc1d3b17cfe39
Glitch (hello-express) <none>
2021-09-13 13:16:21 +0000

    💵👁 Checkpoint ./ayarlar.json:36045533/1487 ./matthe.js:36045533/106

--- matthe.js
+++ matthe.js
@@ -12,7 +12,7 @@ matthe(client)
 var prefix = ayarlar.prefix;
 
 client.on("ready", () => {
-  console.log(`Bot aktif: ${client.user.tag}!`);
+  console.log(`[ MATTHE ] bot başarıyla aktif edildi: ${client.user.tag}!`);
 });
 
 const log = message => {
@@ -25,10 +25,10 @@ client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 fs.readdir("./komutlar/", (err, files) => {
   if (err) console.error(err);
-  log(`${files.length} komut yüklenecek.`);
+  log(`[ MATTHE ] ${files.length} adet komut yüklenecek.`);
   files.forEach(f => {
     let props = require(`./komutlar/${f}`);
-    log(`Yüklenen komut: ${props.help.name}`);
+    log(`[ MATTHE ] yüklenen komut: ${props.help.name}`);
     client.commands.set(props.help.name, props);
     props.conf.aliases.forEach(alias => {
       client.aliases.set(alias, props.help.name);
@@ -113,7 +113,7 @@ client.on("error", e => {
 
 client.on("message", (message) => {
 
-  if (message.content !== "!button" || message.author.bot) return;
+  if (message.content !== "!buton" || message.author.bot) return;
   
   let EtkinlikKatılımcısı = new matthe.MessageButton()
     .setStyle('red') 
