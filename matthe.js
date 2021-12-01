f2d29cdf9d0fc5fbffdd6bb4dc09c0b9f0c0bac8
Glitch (bitter-sticky-juravenator) <none>
2021-12-01 08:26:12 +0000

    💾🍿 Checkpoint ./matthe.js:39251449/66

--- matthe.js
+++ matthe.js
@@ -915,7 +915,7 @@ menu.clicker.member.roles.remove("914992030797676616")
 menu.clicker.member.roles.remove("914994197638053948")
 menu.clicker.member.roles.remove("914994803299725312")
                 break;
-                case "abdurrahman digdık digdık":
+                case "abdülfettah":
                     menu.reply.send("<@&914994197638053948> Rolü verildi", true)
 menu.clicker.member.roles.add("914994197638053948")
 menu.clicker.member.roles.remove("915023584613773333")
