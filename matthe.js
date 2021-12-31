73591e604dd2f69e72e96ffe6450660bc849998a
Glitch (bitter-sticky-juravenator) <none>
2021-12-31 15:31:42 +0000

    🐌👨‍❤️‍💋‍👨 Checkpoint ./matthe.js:38157673/1688

--- matthe.js
+++ matthe.js
@@ -54,6 +54,62 @@ client.reload = command => {
       reject(e);
     }
   });
+};const Discord = require("discord.js");
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const chalk = require("chalk");
+const fs = require("fs");
+const moment = require("moment");
+const Jimp = require("jimp");
+const db = require("quick.db");
+const matthe = require("discord-buttons");
+matthe(client);
+
+var prefix = ayarlar.prefix;
+
+client.on("ready", () => {
+  console.log(`[ MATTHE ] bot başarıyla aktif edildi: ${client.user.tag}!`);
+});
+
+const log = message => {
+  console.log(`[${moment().format("YYYY-MM-DD HH:mm:ss")}] ${message}`);
+};
+
+require("./util/eventLoader")(client);
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`[ MATTHE ] ${files.length} adet komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`[ MATTHE ] yüklenen komut: ${props.help.name}`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 
 client.load = command => {
